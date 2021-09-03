ee11508741d470c1e064b855b041c72634e39a15
Harshit Shekhawat <69758313+Lawapaul@users.noreply.github.com>
2021-09-03 15:08:47 +0530

    Update Travel Agency.py

--- Travel Agency.py
+++ Travel Agency.py
@@ -1,2 +1,57 @@
+print("      F A R E  C A L C U L A T O R      ")
 
+
+
+
+a = int(input("Enter Distance: "))
+b = int(input("Enter no of Children: "))
+c = int(input("Enter no of adults: "))
+
+
+
+if a>=1000 : 
+
+    Fare = 500
+
+    TF = c*500 + b*250 
+
+    print("Total Fare: ",TF)
+
+
+
+
+
+
+
+
+if a<500 : 
+
+    Fare2 = 200
+
+    TF2 = c*200 + b*100
+
+
+    print("Total Fare: ",TF2 )
+
+
+if 500<a<=999 : 
+
+    TF3 = c*300 + b*150
+
+    print("Fare: ",TF3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("      T H A N K   Y O U !     ")
 
