20d747da60c565eb5d11bcbac08bbdbfa1348030
Harshit Shekhawat <69758313+Lawapaul@users.noreply.github.com>
2021-09-03 14:56:32 +0530

    Add files via upload

---
+++ Travel Agency.py
@@ -0,0 +1,63 @@
+
+
+print("      F A R E  C A L C U L A T O R      ")
+
+
+
+
+a = int(input("Enter Distance: "))
+b = int(input("Enter no of Children: "))
+c = int(input("Enter no of adults: "))
+
+
+
+if a>=1000 : 
+
+    Fare = 500
+
+    TF = c*500 + b*250 
+
+    print("Total Fare: ",TF)
+
+
+
+
+
+
+if 500<a<=999 :
+
+
+    Fare1 = 300
+
+    TotalF = c*300 + b*150
+
+
+print("Total Fare: ",TotalF)
+
+
+
+
+if a<500 : 
+
+    Fare2 = 200
+
+    TF2 = c*200 + b*100
+
+
+    print("Total Fare: ",TF2 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("      T H A N K   Y O U !     ")
